93237907c31a661c92d429691e73f6da125e1b3f
tommie1236 <timooosterom@gmail.com>
2022-09-20 17:44:57 +0200

    added not including certian posts

--- new-script.js
+++ new-script.js
@@ -21,22 +21,21 @@ function createCard(gridDiv, cardData) {
     img: 'imgStr'
 }*/
 
-console.log('test');
+
 $.getJSON('https://www.reddit.com/r/persoonlijkebonus/.json', function(data) {
     const posts = data['data']['children'];
     const gridDiv = document.getElementById('gridDiv');
     cards = [];
-    console.log(`posts: ${posts}`);
     for (let i = 0; i < posts.length; i++) {
-        console.log(i);
-        let cardData = {
-            title: posts[i]['data']['title'],
-            url: `https://www.reddit.com/r/persoonlijkebonus/comments/${posts[i]["data"]["id"]}`,
-            /*img: posts[i]['data']['media_metadata'][1] /* [p]/[s]/[u]->([0])->[u] */
+        if (!['MEDEDELING', 'VERLOPEN', 'OVERIG', 'VRAAG'].includes(posts[i]['data']['link_flair_text'])) {
+            let cardData = {
+                title: posts[i]['data']['title'],
+                url: `https://www.reddit.com/r/persoonlijkebonus/comments/${posts[i]["data"]["id"]}`,
+                /*img: posts[i]['data']['media_metadata'][1]['s']['u'].replace('amp;', '')*/
+            };
+            cards.push(createCard(gridDiv, cardData));
         };
-        cards.push(createCard(gridDiv, cardData));
     };
-    console.log(cards);
     getData = document.getElementById('getData');
     getData.remove();
 });
