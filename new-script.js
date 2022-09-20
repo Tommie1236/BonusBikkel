accc4e338a4b6ef6dc564d24e5501d909cb0d47e
tommie1236 <timooosterom@gmail.com>
2022-09-20 22:27:10 +0200

    fixed img's

--- new-script.js
+++ new-script.js
@@ -2,14 +2,14 @@ function createCard(gridDiv, cardData) {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
     cardDiv.setAttribute('onclick', `location.href="${cardData.url}"`);
-    /*const cardImg = document.createElement('img');
+    const cardImg = document.createElement('img');
     cardImg.classList.add('card-img');
-    cardImg.setAttribute('src', cardData.img);*/
+    cardImg.setAttribute('src', cardData.img);
     const cardTitle = document.createElement('h4');
     cardTitle.classList.add('card-title');
     cardTitle.innerHTML = cardData.title;
 
-    /*cardDiv.appendChild(cardImg);*/
+    cardDiv.appendChild(cardImg);
     cardDiv.appendChild(cardTitle);
     gridDiv.appendChild(cardDiv);
     return cardDiv;
@@ -27,12 +27,22 @@ $.getJSON('https://www.reddit.com/r/persoonlijkebonus/.json', function(data) {
     cards = [];
     for (let i = 0; i < posts.length; i++) {
         if (!['MEDEDELING', 'VERLOPEN', 'OVERIG', 'VRAAG'].includes(posts[i]['data']['link_flair_text'])) {
-            let cardData = {
+            try {
+                let imgMeta = posts[i]['data']['media_metadata'];
+                let stripImg = imgMeta[Object.keys(imgMeta)[0]]['s']['u'].replace(/amp;/g, '')
+                var cardData = {
+                    title: posts[i]['data']['title'],
+                    url: `https://www.reddit.com/r/persoonlijkebonus/comments/${posts[i]["data"]["id"]}`,
+                    img: stripImg
+                };
+                console.log(cardData.img);
+            } catch {            
+            var cardData = {
                 title: posts[i]['data']['title'],
-                url: `https://www.reddit.com/r/persoonlijkebonus/comments/${posts[i]["data"]["id"]}`,
-                /*img: posts[i]['data']['media_metadata'][1]['s']['u'].replace('amp;', '')*/
+                url: `https://www.reddit.com/r/persoonlijkebonus/comments/${posts[i]["data"]["id"]}`
             };
-            cards.push(createCard(gridDiv, cardData));
+        };
+        cards.push(createCard(gridDiv, cardData));
         };
     };
     document.getElementById('getData').remove();
